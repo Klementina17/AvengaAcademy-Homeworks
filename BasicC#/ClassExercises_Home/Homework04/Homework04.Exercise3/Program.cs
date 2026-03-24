fc5f6f698e4b6634e16f178abb889ce9cf28f410
Klementina <klementina_dimitrova@live.com>
2026-03-24 19:42:17 +0100

    ClassHomework

---
+++ BasicC#/ClassExercises_Home/Homework04/Homework04.Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿while (true)
+{
+    Console.WriteLine("Enter number: ");
+    bool isParsedNum = int.TryParse(Console.ReadLine(), out int number);
+
+    if (!isParsedNum)
+    {
+        Console.WriteLine("Enter valid number!");
+        continue;
+    }
+    else
+    {
+        string newMess = Substrings(number);
+        Console.WriteLine(newMess);
+        Console.WriteLine("Lenght: " + newMess.Length);
+        break;
+    }
+}
+static string Substrings(int number)
+{
+    string message = "Hello from Avenga Codecademy 2024";
+
+    if (number < 0)
+    {
+        return "Number cannot be negative!";
+    }
+
+    if (number > message.Length)
+    {
+        number = message.Length;
+    }
+    string newMessage = message.Substring(0, number);
+    return newMessage;
+}
+
---
+++ BasicC#/ClassExercises_Home/Homework04/Homework04.Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿DateTime today = DateTime.Today;
+Console.WriteLine(today);
+
+DateTime threeDaysFromNow = today.AddDays(3);
+Console.WriteLine(threeDaysFromNow);
+
+DateTime oneMonthFromNow = today.AddMonths(1);
+Console.WriteLine(oneMonthFromNow);
+
+DateTime oneMonthAndThreeDaysFromNow = oneMonthFromNow.AddDays(3);
+Console.WriteLine(oneMonthAndThreeDaysFromNow);
+
+DateTime oneYearAndTwoMonthsAgo= today.AddYears(-1).AddMonths(-2);
+Console.WriteLine(oneYearAndTwoMonthsAgo);
+
+int currentMonth = today.Month;
+Console.WriteLine("Current Month: " + currentMonth);
+
+int currentYear = today.Year;
+Console.WriteLine("Current year: " + currentYear);
