c41199383e085431521815a92eefba4e1911be01
Klementina <klementina_dimitrova@live.com>
2026-03-24 18:43:25 +0100

    Homework02

---
+++ BasicC#/Homework02/Homework02.StudentGroup/Program.cs
@@ -0,0 +1,55 @@
+﻿// Make a new console application called StudentGroup
+// Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+// Get a number from the console between 1 and 2 and print the students from that group in the console.
+// Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+string []  studentsG1=new string[]{
+    "Klementina",
+    "Kirila",
+    "Adriana",
+    "Dimitar",
+    "Mario"
+};
+string[] studentsG2 = new string[]
+{
+    "Edon",
+    "Ognen",
+    "Damjan",
+    "Teodora",
+    "David"
+};
+
+while (true)
+{
+    Console.WriteLine("Enter student group (1 or 2): ");
+    bool isParsedNum = int.TryParse(Console.ReadLine(), out int number);
+
+    if (!isParsedNum)
+    {
+        Console.WriteLine("Enter valid number");
+        continue;
+    }
+     if (number == 1)
+    {
+        Console.WriteLine("The Students in G1 are:");
+        foreach (string student in studentsG1)
+        {
+            Console.WriteLine(student);
+        }
+        break;
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("The Students in G2 are:");
+        foreach (string student in studentsG2)
+        {
+            Console.WriteLine(student);
+        }
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Enter number between 1 and 2.");
+        continue;
+    }
+
+}
---
+++ BasicC#/Homework02/Homework02.SumOfEven/Program.cs
@@ -0,0 +1,88 @@
+﻿// Make a console application called SumOfEven.
+// Inside it create an array of 6 integers.
+// Get numbers from the input, find and print the sum of the even numbers from the array:
+for (; ; )
+{
+    Console.WriteLine("This will go on forever until you enter X or x!");
+    string input = Console.ReadLine();
+    if (input == "X" || input == "x") break;
+
+    int[] numbers = new int[0];
+    var suma = 0;
+
+    Console.WriteLine("Enter first number: ");
+    bool isParsedFirstNum = int.TryParse(Console.ReadLine(), out int num1);
+
+    if (!isParsedFirstNum)
+    {
+        Console.WriteLine("Enter valid number!");
+    }
+    else
+    {
+        Array.Resize(ref numbers, numbers.Length+1);
+        numbers[0] = num1;
+        Console.WriteLine("Enter second number: ");
+        bool isParsedSecondNum = int.TryParse(Console.ReadLine(), out int num2);
+
+        if (!isParsedSecondNum)
+        {
+            Console.WriteLine("Enter valid number!");
+        }
+        else
+        {
+            Array.Resize(ref numbers, numbers.Length+1);
+            numbers[1] = num2;
+            Console.WriteLine("Enter third number: ");
+            bool isParsedThirdNum = int.TryParse(Console.ReadLine(), out int num3);
+            if (!isParsedThirdNum)
+            {
+                Console.WriteLine("Enter valid number!");
+            }
+            else
+            {
+                Array.Resize(ref numbers, numbers.Length+1);
+                numbers[2] = num3;
+                Console.WriteLine("Enter fourth number: ");
+                bool isParsedFourthNum = int.TryParse(Console.ReadLine(), out int num4);
+
+                if (!isParsedFourthNum)
+                {
+                    Console.WriteLine("Enter valid number!");
+                }
+                else
+                {
+                    Array.Resize(ref numbers, numbers.Length+1);
+                    numbers[3] = num4;
+                    Console.WriteLine("Enter fifth number: ");
+                    bool isParsedFifthNum = int.TryParse(Console.ReadLine(), out int num5);
+                    if (!isParsedFifthNum)
+                    {
+                        Console.WriteLine("Enter valid number!");
+                    }
+                    else
+                    {
+                        Array.Resize(ref numbers, numbers.Length+1);
+                        numbers[4] = num5;
+                        Console.WriteLine("Enter sixth number: ");
+                        bool isParsedSixthNum = int.TryParse(Console.ReadLine(), out int num6);
+                        if (!isParsedSixthNum)
+                        {
+                            Console.WriteLine("Enter valid number!");
+                        }
+                        else
+                        {
+                            Array.Resize(ref numbers, numbers.Length+1);
+                            numbers[5] = num6;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    foreach(int num in numbers)
+    {
+        if(num%2==0) suma += num;
+    }
+    Console.WriteLine("The result is: " + suma);
+}
